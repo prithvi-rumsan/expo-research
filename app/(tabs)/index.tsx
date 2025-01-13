4cd785a838cef62722703b366aa9c77f9f1bac41
prithvi-rumsan <prithvi.maharjan@rumsan.net>
2025-01-13 13:57:23 +0545

    Translation basic setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,54 +1,102 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {
+  Image,
+  StyleSheet,
+  Platform,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { getLocales, getCalendars } from "expo-localization";
+import { useTranslation } from "react-i18next";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import BrazilFlag from "../../assets/images/flags/brazil.png";
+import UsaFlag from "../../assets/images/flags/usa.png";
+import { useEffect } from "react";
 
 export default function HomeScreen() {
+  console.log(getLocales(), getCalendars());
+
+  const { i18n, t } = useTranslation();
+
+  const flags = [
+    { component: BrazilFlag, lang: "pt-BR", name: "Brasil" },
+    { component: UsaFlag, lang: "en-US", name: "USA" },
+  ];
+
+  const currentLanguage = i18n.language;
+
+  useEffect(() => {
+    const loadLanguage = async () => {
+      const savedLanguage = await AsyncStorage.getItem("language");
+      if (savedLanguage) {
+        i18n.changeLanguage(savedLanguage);
+      }
+    };
+    loadLanguage();
+  }, [i18n]);
+
+  const changeLanguage = async (lang: string) => {
+    await AsyncStorage.setItem("language", lang);
+    i18n.changeLanguage(lang);
+  };
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require("@/assets/images/partial-react-logo.png")}
           style={styles.reactLogo}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">{t("home.welcome")}</ThemedText>
         <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
+      <ThemedView>
+        <ThemedText>{t("language")}</ThemedText>
+        <ScrollView>
+          <ThemedView style={styles.flagsContainer}>
+            {flags.map((flag, i) => (
+              <TouchableOpacity
+                key={i}
+                onPress={() => changeLanguage(flag.lang)}
+                style={styles.flag}
+              >
+                <Image
+                  source={flag.component}
+                  style={[
+                    styles.flagIcon,
+
+                    currentLanguage === flag.lang
+                      ? styles.activeFlag
+                      : styles.inactiveFlag,
+                  ]}
+                />
+                <ThemedText
+                  style={[
+                    styles.text,
+                    currentLanguage === flag.lang
+                      ? styles.activeFlag
+                      : styles.inactiveFlag,
+                  ]}
+                >
+                  {flag.name}
+                </ThemedText>
+              </TouchableOpacity>
+            ))}
+          </ThemedView>
+        </ScrollView>
       </ThemedView>
+
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+        {/* <ThemedText type="subtitle">Step 1: Try it</ThemedText> */}
+        <ThemedText>{t("home.description")}</ThemedText>
       </ThemedView>
     </ParallaxScrollView>
   );
@@ -56,8 +104,8 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -69,6 +117,33 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
+  },
+
+  container: {
+    justifyContent: "center",
+  },
+  flagsContainer: {
+    flexDirection: "row",
+    paddingVertical: 10,
+  },
+  flag: {
+    paddingHorizontal: 10,
+  },
+  activeFlag: {
+    transform: [{ scale: 1.2 }],
+  },
+  inactiveFlag: {
+    opacity: 0.5,
+  },
+  text: {
+    fontSize: 22,
+    lineHeight: 32,
+    marginTop: -6,
+  },
+  flagIcon: {
+    width: 40,
+    height: 30,
+    resizeMode: "contain",
   },
 });
---
+++ i18n/index.ts
@@ -0,0 +1,33 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import * as Localization from "expo-localization";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import translationEn from "./locales/en-US.json";
+import translationPt from "./locales/pt-BR.json";
+
+const resources = {
+  "pt-BR": { translation: translationPt },
+  "en-US": { translation: translationEn },
+};
+
+const initI18n = async () => {
+  let savedLanguage = await AsyncStorage.getItem("language");
+
+  if (!savedLanguage) {
+    savedLanguage = Localization.locale;
+  }
+
+  i18n.use(initReactI18next).init({
+    compatibilityJSON: "v3",
+    resources,
+    lng: savedLanguage,
+    fallbackLng: "en-US",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+};
+
+initI18n();
+
+export default i18n;
